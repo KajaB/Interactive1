9ad82c4f845c607039d6540500f33a26dcc02549
KajaB <kberry@cca.edu>
2018-04-28 17:05:23 -0700

    .

---
+++ Project 3/Final/UGH/index.php
@@ -0,0 +1,46 @@
+<link rel="stylesheet" type="text/css" href="styles.css">
+
+<script src="client.js" defer type="text/javascript"></script>
+</head>
+
+<body>
+<h1>Welcome</h1>
+<p>submit comment below</p>
+
+
+
+
+<form action="index.php" method="post">
+<input type="submit" value="Submit">
+</form>
+<textarea rows="4" cols="50" name="comment" form="comment">Enter text here...</textarea>
+
+</body>
+</html>
+
+
+<?php
+
+    if($_POST['submit']){
+      print "<h1>your comment has been submitted<h1>";
+
+      $name = $_POST['name'];
+      $comment = $_POST['comment'];
+
+      #Get old comments
+      $old = fopen("comments.txt", "r+t");
+      $old_comments = fread($old, 1024);
+
+      #delete everything, write down new and old $old_comments
+      $write = fopen("comments.txt", "w+");
+      $string = "<b>" .$name. "</b><br>" .$comment. "<br>\n" .$old_comments;
+      fwrite($write, $string);
+      fclose($write);
+      fclose($old);
+    }
+
+    #read $old_comments
+    $read = fopen("comments.txt", "r+t");
+    echo "<br><br>comments<hr>" .fread($read, 1024);
+    fclose($read);
+?>
